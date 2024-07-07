339fa522317f72dadc805a8f5d8efbc6a266ed99
pramithas123 <pramithas@aruna.software>
2024-07-07 17:47:03 +0545

    Add source map

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,19 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name][contenthash].js'
+        filename: '[name][contenthash].js',
+        clean:true // Do not keep older files.
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port:3000,
+        open:true,
+        hot:true,
+        compress: true,
+        historyApiFallback:true
     },
     module: {
         // css loader
