c2a53a67ca78581e5c6f9f6c698b11a153a550c2
pramithas123 <pramithas@aruna.software>
2024-07-07 17:47:29 +0545

    Add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,16 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     },
